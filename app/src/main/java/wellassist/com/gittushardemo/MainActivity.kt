472c0953c78672d0941e27da971a36cd5e970d4b
Sushant <sushanta@braintechnosys.com>
2022-01-13 12:51:49 +0530

    add all my files t

---
+++ app/src/main/java/wellassist/com/gittushardemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package wellassist.com.gittushardemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
